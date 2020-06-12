52eb1f7574634000ff741994ead4675627d40821
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-06-12 10:31:11 +0200

    Using terminal.writeline

---
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("What would you like to hack?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("Enter your selection ");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
